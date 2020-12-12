bcf6601e1f89a2d731035b131c09357c2417e9b3
Stefan Neuhaus <stefan@stefanneuhaus.org>
2020-12-12 21:17:06 +0100

    Solution to day #01

---
+++ 01/01.hs
@@ -0,0 +1,21 @@
+numbers :: String -> [Int]
+numbers numbersString = map read (lines numbersString)
+
+pairs :: [a] -> [[a]]
+pairs values = [[x,y] | x <- values, y <- values]
+
+triples :: [a] -> [[a]]
+triples values = [[x,y,z] | x <- values, y <- values, z <- values]
+
+sumsUpTo2020 :: [Int] -> Bool
+sumsUpTo2020 numbers = sum numbers == 2020
+
+productOfValuesSummingUpTo2020 :: [[Int]] -> Int
+productOfValuesSummingUpTo2020 values = product (head(filter sumsUpTo2020 values))
+
+
+main = do
+  expensesReport <- readFile "./expense-report.txt"
+  let expenses = numbers expensesReport
+  print (productOfValuesSummingUpTo2020 (pairs expenses))
+  print (productOfValuesSummingUpTo2020 (triples expenses))
